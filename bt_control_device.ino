24bcc44cc4a35567f2955dccc8430dac66e78385
Nimisha Chitriv <80246121+NimishaChitriv@users.noreply.github.com>
2021-04-24 11:08:08 +0530

    Add files via upload

---
+++ bt_control_device.ino
@@ -0,0 +1,29 @@
+/* Home Automation With Bluetooth Module*/
+char data;
+
+void setup()
+{
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while (Serial.available()==0);
+  data = Serial.read();
+  Serial.println(data);
+
+  if (data=='A')
+  digitalWrite(3,HIGH);
+
+  if (data == 'a')
+  digitalWrite(3,LOW);
+
+  if (data=='B')
+  digitalWrite(4,HIGH);
+
+  if (data=='b')
+  digitalWrite(4,LOW);
+}
+
